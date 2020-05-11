3159896e3ba85a4db1978ec398f67988f248fd31
tailonls <tailonlimas@gmail.com>
2020-05-11 20:20:39 -0300

    apagando arquivos desnecessarios

--- src/main/java/core/TestContextConfiguration.java
+++ src/main/java/core/TestContextConfiguration.java
@@ -1,6 +1,17 @@
 package core;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@Configuration
+@ComponentScan("")
+@PropertySource("")
+@Enable
 public class TestContextConfiguration {
+
 
 
 }
